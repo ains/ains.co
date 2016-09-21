1c081338c7ed6ee6881c063ec3f2e5152ac4d919
Ainsley Escorce-Jones <me@ains.co>
2016-09-21 21:16:22 +0100

    Fix config and titles

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 AUTHOR = u'Ainsley Escorce-Jones'
-SITENAME = u'Ainsley Escorce-Jones | Blog'
+SITENAME = u'Ainsley Escorce-Jones | ains.co'
 SITEURL = '/'
 
 TIMEZONE = 'Europe/Paris'
@@ -14,16 +14,6 @@ DEFAULT_LANG = u'en'
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
-
-# Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
 
 DEFAULT_PAGINATION = 10
 
--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://ains.co/blog/'
+SITEURL = 'http://ains.co/'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
